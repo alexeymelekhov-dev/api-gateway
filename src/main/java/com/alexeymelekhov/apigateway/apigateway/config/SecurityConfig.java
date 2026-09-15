6c078e7e107e2e03fb3d0c1fe41623db2521bc5c
leshasmp <leshasmp1989@mail.ru>
2026-09-15 14:55:29 +0500

    chore: disable csrf for resource auth

--- src/main/java/com/alexeymelekhov/apigateway/apigateway/config/SecurityConfig.java
+++ src/main/java/com/alexeymelekhov/apigateway/apigateway/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +18,7 @@ public class SecurityConfig {
     SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity http) {
         http
                 .securityMatcher("/api/**")
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
