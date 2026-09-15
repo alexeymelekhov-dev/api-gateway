9317b3c3f39cea8652498584c59393c3f3797275
leshasmp <leshasmp1989@mail.ru>
2026-09-15 14:49:45 +0500

    refactor: split OAuth2 security filter chains

--- src/main/java/com/alexeymelekhov/apigateway/apigateway/config/SecurityConfig.java
+++ src/main/java/com/alexeymelekhov/apigateway/apigateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.alexeymelekhov.apigateway.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,10 +13,26 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    @Order(1)
+    SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity http) {
+        http
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain loginSecurityFilterChain(HttpSecurity http) {
         http
                 .authorizeHttpRequests(auth -> auth
-                    .anyRequest().authenticated()
+                        .anyRequest().authenticated()
                 )
                 .oauth2Login(Customizer.withDefaults());
 
